e157d021ebc6829aae1c0320412852e72c85cd32
root <root@localhost.localdomain>
2020-04-05 14:54:28 -0400

    Updated DB & added .gitignore

--- MySite/settings.py
+++ MySite/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'MySite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'homepage',
-        'USER': 'masterSloth',
-        'PASSWORD': 'aSSd!!2324',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
 
