1d56ccfa3e1ddf155bd5da4201e18cd46cf27ca7
root <root@localhost.localdomain>
2020-04-05 15:17:02 -0400

    Implemented base.html and added block content tags for other pages

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# Our first test will check to ensure that our site is up and running and displays our homepage
+
+
+class TestPage(TestCase):
+    def test_home_page_works(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertContains(response, 'MySite')
